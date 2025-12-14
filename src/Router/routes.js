5f63dda2ea062f0b4dfdadb6922305a0823900bc
JavierXool <javier.xool@outlook.com>
2025-12-13 23:54:04 -0600

    Configuracion de Vite para enrutamiento

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -3,7 +3,7 @@ import HomePage from "@/components/Home/HomePage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
